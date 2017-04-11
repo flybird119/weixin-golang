440a79eb33dddf861649dd459b1fbc2cbcb4fd17
Wang-Kai <persistence2013@sina.com>
2017-04-11 20:43:40 +0800

    add refresh_urls interface

--- gateway/controller/mediastore.go
+++ gateway/controller/mediastore.go
@@ -11,3 +11,8 @@ func GetUplaodToken(w http.ResponseWriter, r *http.Request) {
 	req := &pb.UpLoadReq{}
 	misc.CallWithResp(w, r, "bc_mediastore", "GetUpToken", req, "key")
 }
+
+func RefreshUrls(w http.ResponseWriter, r *http.Request) {
+	req := &pb.RefreshReq{}
+	misc.CallWithResp(w, r, "bc_mediastore", "RefreshUrls", req, "urls")
+}
