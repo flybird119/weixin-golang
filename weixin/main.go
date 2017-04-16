af298ee51f1e7af7d38dc6cc137b1a3b83d8b5c9
Wang-Kai <persistence2013@sina.com>
2017-04-16 18:00:10 +0800

    add weixin module

---
+++ weixin/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/goushuyun/weixin-golang/db"
+	"github.com/goushuyun/weixin-golang/pb"
+	"github.com/goushuyun/weixin-golang/weixin/service"
+	"github.com/wothing/worpc"
+	"google.golang.org/grpc"
+)
+
+const (
+	svcName = "bc_weixin"
+	port    = 8858
+)
+
+var svcNames = []string{}
+
+func main() {
+	m := db.NewMicro(svcName, port)
+	m.RegisterPG()
+	m.ReferServices(svcNames...)
+
+	s := grpc.NewServer(grpc.UnaryInterceptor(worpc.UnaryInterceptorChain(worpc.Recovery, worpc.Logging)))
+	pb.RegisterWeixinServiceServer(s, &service.WeixinServer{})
+	s.Serve(m.CreateListener())
+}
