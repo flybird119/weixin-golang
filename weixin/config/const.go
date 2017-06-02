51aa3bb1dbac82fc0c639d86ef7ed38935cf8964
Wang-Kai <persistence2013@sina.com>
2017-06-02 12:25:07 +0800

    AppID and AppSecret get from etcd

--- weixin/config/const.go
+++ weixin/config/const.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"github.com/goushuyun/weixin-golang/db"
+)
+
 type Conf struct {
 	AppID     string
 	AppSecret string
@@ -9,11 +13,17 @@ type Conf struct {
 
 var conf *Conf
 
+const (
+	svcName = "weixin"
+)
+
 func GetConf() *Conf {
+	// get AppID & AppSecret from etcd
+
 	if conf == nil {
 		return &Conf{
-			AppID:     "wx1c2695469ae47724",
-			AppSecret: "bd8f125dc0300451e7495c70f5480575",
+			AppID:     db.GetValue(svcName, "/component/AppID", "wx1c2695469ae47724"),
+			AppSecret: db.GetValue(svcName, "/component/AppSecret", "bd8f125dc0300451e7495c70f5480575"),
 			Token:     "goushuyun",
 			AESKey:    "goushuyungoushuyungoushuyungoushuyungoushuy",
 		}
